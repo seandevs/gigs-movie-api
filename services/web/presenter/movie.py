4d07369a88f2d49d8ab85f6b5487cf9aede666b0
Sean Devlin <seanpdevlin@gmail.com>
2022-10-27 12:55:09 -0400

    Add movie presenter object for returning json in API

---
+++ services/web/presenter/movie.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from datetime import date
+
+
+@dataclass
+class MovieClass:
+    id_: int
+    name: str
+    release_date: date
+    imdb_rating: float
+    runtime: str
+
+    def json(self):
+        return {
+                'id': self.id_,
+                'name': self.name,
+                'release_date': self.release_date,
+                'imdb_rating': self.imdb_rating,
+                'runtime': self.runtime
+            }
