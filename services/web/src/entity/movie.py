00ae5bfade9ee303c3b91aefac7ef02f403c1892
Sean Devlin <seanpdevlin@gmail.com>
2022-10-27 12:47:16 -0400

    Add movie entity

---
+++ services/web/src/entity/movie.py
@@ -0,0 +1,11 @@
+from src import db
+
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    release_date = db.Column(db.Date)
+    imdb_rating = db.Column(db.Float)
+    runtime = db.Column(db.String(50))
